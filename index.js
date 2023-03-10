3b73ac931099398a6900c333b4c3856d1c86e73e
Jordan Pace <jordanpace95@gmail.com>
2023-03-09 22:13:24 -0700

    adds switch statements

--- index.js
+++ index.js
@@ -27,83 +27,95 @@ const db = mysql.createConnection(
 
 
 (async function init() {
-  await loadPrompts();
+    await loadPrompts();
 })();
 async function loadPrompts() {
-  const { choice } = await inquirer.prompt([
-    {
-      type: "list",
-      name: "choice",
-      message: "What would you like to do?",
-      choices: [
+    const { choice } = await inquirer.prompt([
         {
-          name: "View All Employees",
-          value: "VIEW_EMPLOYEES",
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "VIEW_EMPLOYEES",
+                },
+                {
+                    name: "View All Employees By Department",
+                    value: "VIEW_EMPLOYEES_BY_DEPARTMENT",
+                },
+                {
+                    name: "Add Employee",
+                    value: "ADD_EMPLOYEE",
+                },
+                {
+                    name: "Remove Employee",
+                    value: "REMOVE_EMPLOYEE",
+                },
+                {
+                    name: "Update Employee Role",
+                    value: "UPDATE_EMPLOYEE_ROLE",
+                },
+                {
+                    name: "View All Roles",
+                    value: "VIEW_ROLES",
+                },
+                {
+                    name: "Add Role",
+                    value: "ADD_ROLE",
+                },
+                {
+                    name: "Remove Role",
+                    value: "REMOVE_ROLE",
+                },
+                {
+                    name: "View All Departments",
+                    value: "VIEW_DEPARTMENTS",
+                },
+                {
+                    name: "Add Department",
+                    value: "ADD_DEPARTMENT",
+                },
+                {
+                    name: "Remove Department",
+                    value: "REMOVE_DEPARTMENT",
+                },
+                {
+                    name: "Quit",
+                    value: "QUIT",
+                },
+            ],
         },
-        {
-          name: "View All Employees By Department",
-          value: "VIEW_EMPLOYEES_BY_DEPARTMENT",
-        },
-        {
-          name: "View All Employees By Manager",
-          value: "VIEW_EMPLOYEES_BY_MANAGER",
-        },
-        {
-          name: "Add Employee",
-          value: "ADD_EMPLOYEE",
-        },
-        {
-          name: "Remove Employee",
-          value: "REMOVE_EMPLOYEE",
-        },
-        {
-          name: "Update Employee Role",
-          value: "UPDATE_EMPLOYEE_ROLE",
-        },
-        {
-          name: "Update Employee Manager",
-          value: "UPDATE_EMPLOYEE_MANAGER",
-        },
-        {
-          name: "View All Roles",
-          value: "VIEW_ROLES",
-        },
-        {
-          name: "Add Role",
-          value: "ADD_ROLE",
-        },
-        {
-          name: "Remove Role",
-          value: "REMOVE_ROLE",
-        },
-        {
-          name: "View All Departments",
-          value: "VIEW_DEPARTMENTS",
-        },
-        {
-          name: "Add Department",
-          value: "ADD_DEPARTMENT",
-        },
-        {
-          name: "Remove Department",
-          value: "REMOVE_DEPARTMENT",
-        },
-        {
-          name: "Quit",
-          value: "QUIT",
-        },
-      ],
-    },
-  ]);
-  switch (choice) {
-    case "VIEW_EMPLOYEES":
-      return viewEmployees();
-    default:
-      return quit();
-  }
+    ]);
+    switch (choice) {
+        case "VIEW_EMPLOYEES":
+            return viewEmployees();
+        case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+            return viewEmployees();
+        case "ADD_EMPLOYEE":
+            return viewEmployees();
+        case "REMOVE_EMPLOYEE":
+            return viewEmployees();
+        case "UPDATE_EMPLOYEE_ROLE":
+            return viewEmployees();
+        case "VIEW_ROLES":
+            return viewEmployees();
+        case "ADD_ROLE":
+            return viewEmployees();
+        case "REMOVE_ROLE":
+            return viewEmployees();
+        case "VIEW_DEPARTMENTS":
+            return viewEmployees();
+        case "ADD_DEPARTMENT":
+            return viewEmployees();
+        case "REMOVE_DEPARTMENT":
+            return viewEmployees();
+        default:
+            return quit();
+    }
 }
 async function viewEmployees() {
-  const employees = await db.findAllEmployees();
-  console.table(employees);
-  loadPrompts();
+    const employees = await db.findAllEmployees();
+    console.table(employees);
+    loadPrompts();
 }
